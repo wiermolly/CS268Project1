5740ca905bdd7d42074df8c2572b05478340c517
Molly Wier <wiermm1019@uwec.edu>
2021-05-05 16:55:59 -0500

    added db functionality

---
+++ project/php/ast.php
@@ -0,0 +1,22 @@
+<?php
+    $fname = $_POST['fname'];
+    $lname = $_POST['lname'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $num = $_POST['num'];
+    $date = $_POST['date'];
+
+    //database connection
+    $conn = new mysqli('localhost', 'root', '', 'project');
+    if($conn->connect_error){
+        die('Connection failed : ' .$conn->connect_error);
+    }else{
+        $stmt = $conn->prepare("INSERT INTO appointments (lesson_id, fname, lname, email, phone, num_people, day) values (?,?,?,?,?,?,?);");
+        $stmt->bind_param("issssis", 1, $fname, $lname, $email, $phone, $num, null);
+        $stmt->execute();
+        echo "appointment added";
+        $stmt->close();
+        $conn->close();
+        }
+
+?>
